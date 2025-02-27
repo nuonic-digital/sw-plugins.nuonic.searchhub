2000964cb8f4739135cf42efd9f0eac3ab6fab94
Rillox, Benedikt <benedikt@nuonic.de>
2025-02-27 15:03:58 +0100

    SHI-3:  Init base plugin

---
+++ src/Config/PluginConfigService.php
@@ -0,0 +1,98 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NuonicSearchHubIntegration\Config;
+
+use Shopware\Core\System\SystemConfig\Exception\InvalidSettingValueException;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
+
+readonly class PluginConfigService
+{
+    public function __construct(
+        private SystemConfigService $systemConfigService,
+        private string $configDomain,
+    ) {
+    }
+
+    /**
+     * @return array<mixed>|bool|float|int|string|null
+     */
+    public function get(string $key, ?string $salesChannelId = null, ?bool $inherit = true)
+    {
+        $result = $this->systemConfigService->get(
+            sprintf('%s.%s', $this->configDomain, $key),
+            $salesChannelId
+        );
+
+        if (is_null($result) && $inherit && is_string($salesChannelId)) {
+            return $this->get($key);
+        }
+
+        return $result;
+    }
+
+    public function getString(string $key, ?string $salesChannelId = null, ?bool $inherit = true): string
+    {
+        try {
+            return $this->systemConfigService->getString(
+                sprintf('%s.%s', $this->configDomain, $key),
+                $salesChannelId
+            );
+        } catch (InvalidSettingValueException $e) {
+            if ($inherit && is_string($salesChannelId)) {
+                return $this->getString($key);
+            }
+
+            throw $e;
+        }
+    }
+
+    public function getInt(string $key, ?string $salesChannelId = null, ?bool $inherit = true): int
+    {
+        try {
+            return $this->systemConfigService->getInt(
+                sprintf('%s.%s', $this->configDomain, $key),
+                $salesChannelId
+            );
+        } catch (InvalidSettingValueException $e) {
+            if ($inherit && is_string($salesChannelId)) {
+                return $this->getInt($key);
+            }
+
+            throw $e;
+        }
+    }
+
+    public function getFloat(string $key, ?string $salesChannelId = null, ?bool $inherit = true): float
+    {
+        try {
+            return $this->systemConfigService->getFloat(
+                sprintf('%s.%s', $this->configDomain, $key),
+                $salesChannelId
+            );
+        } catch (InvalidSettingValueException $e) {
+            if ($inherit && is_string($salesChannelId)) {
+                return $this->getFloat($key);
+            }
+
+            throw $e;
+        }
+    }
+
+    public function getBool(string $key, ?string $salesChannelId = null, ?bool $inherit = true): bool
+    {
+        try {
+            return $this->systemConfigService->getBool(
+                sprintf('%s.%s', $this->configDomain, $key),
+                $salesChannelId
+            );
+        } catch (InvalidSettingValueException $e) {
+            if ($inherit && is_string($salesChannelId)) {
+                return $this->getBool($key);
+            }
+
+            throw $e;
+        }
+    }
+}
---
+++ src/NuonicSearchHubIntegration.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NuonicSearchHubIntegration;
+
+use Shopware\Core\Framework\Plugin;
+
+class NuonicSearchHubIntegration extends Plugin
+{
+}
